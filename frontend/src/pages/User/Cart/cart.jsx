dec9790729c6fbb4e2943c10a467330b05463a61
SaiSudha Gadre <saisudhagadrewg542@gmail.com>
2024-08-12 22:45:15 -0400

    cart and order

--- frontend/src/pages/User/Cart/cart.jsx
+++ frontend/src/pages/User/Cart/cart.jsx
@@ -1,27 +1,46 @@
 
 import { Link, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import {Card,Row,Col,Container} from 'react-bootstrap';
 import './cart.css';
 
 const Cart = ()=>{
     const cart = useSelector((state)=>state.cart);
-    const {cartItems} = cart;
+    const {cartItems,itemsPrice} = cart;
+    const navigate = useNavigate();
+
+    const onCheckOut = async(e)=>{
+        navigate('/order');
+    }  
    return (
     <>
     <div>
         {cartItems.length===0?
         (<div> Cart is Empty  </div>): 
-        (<div>
-            <h1>Shopping Cart</h1>
+        (<div className="cart-info">
+            <h2>Shopping Cart</h2>
             {cartItems.map((item)=>
-             <div key={item._id}>
-                 <img src={item.image} alt={item.moviename}></img>
-                 <div></div>
+             <div key={item._id} className="cart-detail">
+                 <img src={item.image} alt={item.moviename} className="cart-detail-image"/>
+                 <div className="cart-detail-info">
+                 <h3>{item.moviename}</h3>
+                 <div className="cart-detail-indetail">Genre: {item.genre}</div>
+                 <div className="cart-detail-indetail">Language: {item.language}</div>               
+                 <div className="cart-detail-indetail">Rating:{item.overallrating}</div>
+                 <div className="cart-detail-indetail">Reviews: {item.numReviews}</div>
+                 </div>
+                 <div className="cart-detail-price">
+                   <h3> ${item.price}</h3> 
+                </div>
             </div>
             )}
-           
-
+            <div>
+                <h4 className="cart-detail-total-price">Total price: {itemsPrice}</h4>
+            <button             
+              type="submit"
+              className="cart-detail-button" onClick={onCheckOut}>
+            Proceed To Checkout
+            </button>
+            </div>
         </div>)}
     </div>
     </>

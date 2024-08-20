b1ce9ce052ba33b2ee16042105b4172dcd3e8b57
SaiSudha Gadre <saisudhagadrewg542@gmail.com>
2024-08-19 22:07:52 -0400

    ui

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -6,11 +6,14 @@ function Home() {
 
   return (
     <>
-      <div className='home'>
-      Welcome to CineBliss!
-      Your Ultimate Destination for Movies
-      At CineBliss, we bring you the best in cinematic entertainment. Whether you're in the mood for a blockbuster hit, an indie gem, or a classic favorite, we've got you covered. Explore our vast collection and choose how you want to enjoy your movies: for free, by renting, or by buying.
-      </div>
+      <div className='home'>    
+        <h1>Welcome to CineBliss!<br/></h1>
+        <p>
+          Your Ultimate Destination for Movies.
+          <br/><br/>
+          At CineBliss, we bring you the best in cinematic entertainment. Whether you're in the mood for a blockbuster hit, an indie gem, or a classic favorite, we've got you covered. Explore our vast collection and choose how you want to enjoy your movies for free, by renting, or by buying.
+        </p>
+        </div>
     </>
   )
 }
